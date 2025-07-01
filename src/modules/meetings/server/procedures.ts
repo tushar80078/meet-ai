91b4ab99cfc272f5fb8dc0157c06f3f798b794d1
Tushar Bhosale <tusharbhosale2281@gmail.com>
2025-07-01 11:04:27 +0530

    Added procedures

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -14,6 +14,25 @@ import { meetingsInsertSchema, meetingsUpdatetSchema } from "../schema";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const [removeMeeting] = await db
+        .delete(meetings)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!removeMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return removeMeeting;
+    }),
   update: protectedProcedure
     .input(meetingsUpdatetSchema)
     .mutation(async ({ input, ctx }) => {
@@ -60,8 +79,13 @@ export const meetingsRouter = createTRPCRouter({
 
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM(ended_at - started_at))`.as(
+            "duration"
+          ),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
         );
